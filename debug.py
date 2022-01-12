9348a634448595b815e092225c79419fb145d40f
FrankQixin <qd@ualberta.ca>
2022-01-11 22:08:30 -0700

    1

--- debug.py
+++ debug.py
@@ -1,22 +1,28 @@
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
+
 class Solution:
-    def isValid(self, s: str) -> bool:
-        stack=list()
-        if len(s)%2==1:
-            return False
-        dict={
-            ")":"(",
-            "]":"[",
-            "}":"{"
-        }
-        for char in s:
-            if char in dict :
-                if stack[-1]!=dict[char]:
-                    return False
-                else:
-                    stack.pop()
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        if not head or not head.next:
+            return head
+        l1=ListNode(0)
+        cur_l1=l1
+        l2=ListNode(0)
+        cur_l2=l2
+        
+        cur =head
+        while cur.next!=None:
+            if cur.val<x:
+                cur_l1.next=cur
+                cur_l1=cur_l1.next
             else:
-                stack.append(char)
-        return len(stack)==0
+                cur_l2.next=cur
+                cur_l2=cur_l2.next
+            cur=cur.next      
+        cur_l1.next=l2.next
+        return l1.next
 
 sol=Solution()
-print(sol.isValid("}("))
+print(sol.partition([1,4,3,2,5,2],3))
